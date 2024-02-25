abaf403b7b122c1d4955d6500482bedcdc67eed8
Pradyumna Bhat <122141906+pradyumna100903@users.noreply.github.com>
2024-02-25 11:33:15 +0530

    Create Reach a Given Score.java

---
+++ Reach a Given Score.java
@@ -0,0 +1,17 @@
+class Geeks {
+    public long count(int n) {
+         int[] table = new int[n + 1];
+        Arrays.fill(table, 0);
+        table[0] = 1;
+        for(int i = 3; i <= n; i++){
+            table[i] += table[i - 3];
+        }
+        for(int i = 5; i <= n; i++){
+            table[i] += table[i - 5];
+        }
+        for(int i = 10; i <= n; i++){
+            table[i] += table[i - 10];
+        }
+        return table[n];
+    }
+}
